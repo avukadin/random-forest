6398180395d10c28d153081c0e1168ffc6fd8ee8
Alex Vukadinovic <alex@Alexs-MacBook-Air.local>
2021-11-08 10:20:42 -0500

    export

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { RandomForestClassifier } from './RandomForestClassifier';
 export { RandomForestRegression } from './RandomForestRegression';
+export { RandomForestBase } from './RandomForestBase';
